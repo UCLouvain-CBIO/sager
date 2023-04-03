748691614ec44d4431d640569ac0496eefa3d8f7
Laurent Gatto <laurent.gatto@uclouvain.be>
2023-04-03 10:18:55 +0200

    replace sageSummarizedExperiment with sageQFeatures

--- R/sageQFeatures.R
+++ R/sageQFeatures.R
@@ -4,7 +4,7 @@
 ##'
 ##' This function imports sage TMT quantitation and identification
 ##' results and combines them into a
-##' [SummarizedExperiment::SummarizedExperiment()] object.
+##' [QFeatures::QFeatures()] object.
 ##'
 ##' @param quantFile `character(1)` containing the quantification
 ##'     results, typically "quant.tsv".
@@ -21,12 +21,24 @@
 ##'     identification columns used for merging. Passed the [merge()]
 ##'     as `by.y`. Default is `c("filename", "scannr")`.
 ##'
+##' @param splitBy A character refering the a `quantFile` or `idFile`
+##'     column that will be used to split the merged table into
+##'     different assays, i.e. acquisitions corresponding to different
+##'     sets of samples. Default is to use `byQuant[1]`. Set to `NULL`
+##'     to not split and return a result with a single assay, for
+##'     instance if several fractions from one sample were used.
+##'
 ##' @param quantPattern `character(1)` defining the pattern passed to
 ##'     [grep()] to extract the columns containing quantitative data.
 ##'
+##' @param class `character(1)` with pne of `"SummarizedExperiment"`
+##'     or `"SingleCellExperiment"` defining the assay's
+##'     class. Default is the former.
+##'
 ##' @param ... Additional parameters passed to [read.delim()].
 ##'
-##' @return An instance of class [SummarizedExperiment()].
+##' @return An instance of class [QFeatures()] with as many assays as
+##'     defined by `splitBy`.
 ##'
 ##' @author Laurent Gatto
 ##'
@@ -34,7 +46,7 @@
 ##'
 ##' @importFrom utils read.delim
 ##'
-##' @importFrom QFeatures readSummarizedExperiment
+##' @importFrom QFeatures readSummarizedExperiment QFeatures
 ##'
 ##' @examples
 ##'
@@ -49,13 +61,24 @@
 sageQFeatures <- function(quantFile, idFile,
                           byQuant = c("file", "scannr"),
                           byId = c("filename", "scannr"),
+                          splitBy = byQuant[1],
                           quantPattern = "tmt_",
+                          class = c("SummarizedExperiment", "SingleCellExperiment"),
                           ...) {
-
+    stopifnot(suppressPackageStartupMessages(require("QFeatures")))
+    class <- match.arg(class)
     quant <- read.delim(quantFile, ...)
     id <- read.delim(idFile, ...)
     x <- merge(quant, id, by.x = byQuant, by.y = byId)
-    res <- split(x, x[, byQuant[1]])
-    QFeatures(lapply(res, readSummarizedExperiment,
-                     ecol = ecol <- grep("tmt", names(x))))
+    ecol <- grep(quantPattern, names(x))
+    if (!is.null(splitBy)) {
+        x <- split(x, x[, splitBy])
+    } else {
+        x <- list(sage = x)
+    }
+    if (class == "SingleCellExperiment") {
+        requireNamespace("scp")
+        return(QFeatures(lapply(x, scp::readSingleCellExperiment, ecol = ecol)))
+    }
+    QFeatures(lapply(x, QFeatures::readSummarizedExperiment, ecol = ecol))
 }
--- inst/scripts/make-data.R
+++ inst/scripts/make-data.R
@@ -1,16 +1,16 @@
 library("BiocFileCache")
 
-quantFile <- "~/wrk/sager/inst/extdata/quant.tsv"
+quantFile <- "~/dev/sager/inst/extdata/quant.tsv"
 bfcadd(BiocFileCache(), "sageQuantExample", quantFile)
 
-sageFile <- "~/wrk/sager/inst/extdata/results.sage.tsv"
+sageFile <- "~/dev/sager/inst/extdata/results.sage.tsv"
 bfcadd(BiocFileCache(), "sageResultExample", sageFile)
 
 
-rawFiles <- c("/home/lgatto/wrk/sager/inst/extdata/CBIO944_1.mzML",
-              "/home/lgatto/wrk/sager/inst/extdata/CBIO944_2.mzML",
-              "/home/lgatto/wrk/sager/inst/extdata/CBIO944_3.mzML",
-              "/home/lgatto/wrk/sager/inst/extdata/CBIO944_4.mzML")
+rawFiles <- c("/home/lgatto/dev/sager/inst/extdata/CBIO944_1.mzML",
+              "/home/lgatto/dev/sager/inst/extdata/CBIO944_2.mzML",
+              "/home/lgatto/dev/sager/inst/extdata/CBIO944_3.mzML",
+              "/home/lgatto/dev/sager/inst/extdata/CBIO944_4.mzML")
 bfcadd(BiocFileCache(), "sageRawExample", rawFiles)
 
 library(Spectra)
