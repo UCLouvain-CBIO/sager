197ba9c50d55171ba438dc981473d86173a02be1
Laurent Gatto <laurent.gatto@uclouvain.be>
2023-04-05 11:38:29 +0200

    update make-date.R to use PXD016766

--- inst/scripts/make-data.R
+++ inst/scripts/make-data.R
@@ -1,37 +1,12 @@
-## library("BiocFileCache")
-
-## quantFile <- "~/dev/sager/inst/extdata/quant.tsv"
-## bfcadd(BiocFileCache(), "sageQuantExample", quantFile)
-
-## sageFile <- "~/dev/sager/inst/extdata/results.sage.tsv"
-## bfcadd(BiocFileCache(), "sageResultExample", sageFile)
-
-
-## rawFiles <- c("/home/lgatto/dev/sager/inst/extdata/CBIO944_1.mzML",
-##               "/home/lgatto/dev/sager/inst/extdata/CBIO944_2.mzML",
-##               "/home/lgatto/dev/sager/inst/extdata/CBIO944_3.mzML",
-##               "/home/lgatto/dev/sager/inst/extdata/CBIO944_4.mzML")
-## bfcadd(BiocFileCache(), "sageRawExample", rawFiles)
-
-## library(Spectra)
-## sageMzML <- Spectra(rawFiles)
-## bfc <- BiocFileCache()
-## savepath <- bfcnew(bfc, "sageMzML", ext=".RData")
-## save(sageMzML, file = savepath)
-
-
-## =========================================
-
-
 ## --------------------------------------------------------------------
 ## Get the sage parameter file to run the seach (see below) and
 ## extract the mzML files used for that benchmark
 
 library(jsonlite)
-download.file("https://raw.githubusercontent.com/lazear/sage/master/figures/benchmark_params/tmt.json",
-              dest = "../extdata/tmt.json")
 
-mzml_files <- fromJSON("../extdata/tmt.json")$mzml_paths
+download.file("https://raw.githubusercontent.com/lazear/sage/master/figures/benchmark_params/tmt.json",
+              dest = "tmt.json")
+mzml_files <- fromJSON("tmt.json")$mzml_paths
 
 ## --------------------------------------------------------------------
 ## Get the raw files from the ProteomeXchange project
@@ -42,51 +17,58 @@ raw_files <- sub(".mzML", ".raw", mzml_files)
 f <- pxget(px, raw_files)
 
 ## --------------------------------------------------------------------
-## Convert the raw files to mzML. These mzML files are stored in a
-## temporary directory before being cached using rpx.
+## Convert the raw files to mzML using ThermoRawFileParser. These mzML
+## files are stored in a temporary directory before being cached using
+## BiocFileCache.
 
-tmpdir <- tempdir()
 mzml_dest <- file.path(tmpdir, sub("raw", "mzML", basename(f)))
-
 convert <- paste0("mono ~/bin/ThermoRawFileParser/1.4.2/ThermoRawFileParser.exe",
                   " -i=", f,
                   " -b=", mzml_dest,
                   " -f=2", ## indexed mzML
                   " -p=1") ## no peak picking for MS level 1
-
 sapply(convert, system)
 
 ## --------------------------------------------------------------------
-## Add the mzML files to the rpx cache
+## Add the mzML files to the rpx cache using the BiocFileCache
+## package. The cached locations are then queried and stored in
+## mzml_rpath.
 
 library(BiocFileCache)
+rpx_cache <- rpx::rpxCache()
 
-rpx_cache <- rpxCache()
-
-## bfcadd(rpx_cache,
-##        rname = basename(mzml_dest),
-##        fpath = mzml_dest,
-##        action = "copy")
+bfcadd(rpx_cache,
+       rname = basename(mzml_dest),
+       fpath = mzml_dest,
+       action = "copy")
 
 mzml_rpath <- bfcquery(rpx_cache, "11cell_90min_hrMS2.+\\.mzML", exact = FALSE)$rpath
 
 ## --------------------------------------------------------------------
 ## Update the sage config with new mzML filenames and run it
 
-config <- fromJSON("../extdata/tmt.json")
+config <- fromJSON("tmt.json")
 config$mzml_paths <- mzml_rpath
 config$database$fasta <- "/mnt/isilon/CBIO/data/SCPCBIO/fasta/UP000005640_9606.fasta"
-config$output_directory <- "output2"
+config$database$missed_cleavages <- 2
+config$database$fragment_min_mz <- 100
+config$database$variable_mods$'$' <- 49.2022
+config$database$variable_mods$'[' <- 42.0
+config$database$variable_mods$']' <- 111.0
+config$database$variable_mods$'M' <- 15.9949
+config$database$generate_decoys <- TRUE
+config$database$max_variable_mods <- 2
+config$database$decoy_tag <- "rev_"
+config$output_directory <- "sage_output"
 config$process_files_parallel <- NULL
 config$chimera <- TRUE
 config$quant$tmt <- "Tmt11"
 config$quant$tmt_level <- 2
-config$quant$sn <- TRUE
-toJSON(config, auto_unbox = TRUE) |> writeLines("../extdata/tmt2.json")
+toJSON(config, auto_unbox = TRUE, pretty = TRUE) |>
+    writeLines("tmt2.json")
 
-system("~/bin/sage/sage-0.8.1/target/release/sage ../extdata/tmt2.json")
+system("~/bin/sage/sage-v0.10.0-x86_64-unknown-linux-gnu/sage tmt2.json")
 
-## system("~/dev/sage/target/release/sage ../extdata/tmt2.json")
 
 
 ## --------------------------------------------------------------------
@@ -94,6 +76,7 @@ system("~/bin/sage/sage-0.8.1/target/release/sage ../extdata/tmt2.json")
 library(sager)
 
 x <- sageQFeatures("output2/quant.tsv", "output2/results.sage.tsv")
+x
 
 psm <- sagePSM("output2/results.sage.tsv")
 psm
