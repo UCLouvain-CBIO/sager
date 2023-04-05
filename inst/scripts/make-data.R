6f2f5b57f079c139f06bd40a945728c84df02929
Laurent Gatto <laurent.gatto@uclouvain.be>
2023-04-05 16:12:56 +0200

    create sage results subset

--- inst/scripts/make-data.R
+++ inst/scripts/make-data.R
@@ -8,6 +8,7 @@ download.file("https://raw.githubusercontent.com/lazear/sage/master/figures/benc
               dest = "tmt.json")
 mzml_files <- fromJSON("tmt.json")$mzml_paths
 
+
 ## --------------------------------------------------------------------
 ## Get the raw files from the ProteomeXchange project
 library(rpx)
@@ -20,6 +21,8 @@ f <- pxget(px, raw_files)
 ## Convert the raw files to mzML using ThermoRawFileParser. These mzML
 ## files are stored in a temporary directory before being cached using
 ## BiocFileCache.
+
+tmpdir <- tempdir()
 
 mzml_dest <- file.path(tmpdir, sub("raw", "mzML", basename(f)))
 convert <- paste0("mono ~/bin/ThermoRawFileParser/1.4.2/ThermoRawFileParser.exe",
@@ -70,8 +73,8 @@ toJSON(config, auto_unbox = TRUE, pretty = TRUE) |>
 system("~/bin/sage/sage-v0.10.0-x86_64-unknown-linux-gnu/sage tmt2.json")
 
 ## --------------------------------------------------------------------
-## Store sage results to the rpx cache using BiocFileCache. The cached
-## locations are then queried and stored in sager_rpath.
+## Add the sage results to the rpx cache using BiocFileCache. The
+## cached locations are then queried and stored in sager_rpath.
 
 sage_results <- dir("sage_output", full.names = TRUE)
 
@@ -82,3 +85,61 @@ bfcadd(rpx_cache,
 
 sager_rpath <- c(quant = bfcquery(rpx_cache, "sager_quant")$rpath,
                  id = bfcquery(rpx_cache, "sager_results.sage.tsv")$rpath)
+
+## --------------------------------------------------------------------
+## Create smaller data and add them to the rpx cache. The subset is a
+## selectiob of 5000 forward PSMs (label of 1), with a spectrum FDR <
+## 0.01, and a rank of 1).
+
+
+## Subset sage identification results results.sage.tsv
+
+set.seed(123)
+sager_id2 <- read_tsv(sager_rpath["id"]) |>
+    filter(label > 0) |>
+    filter(spectrum_fdr < 0.01) |>
+    filter(rank == 1) |>
+    sample_n(5000)
+
+write_tsv(sager_id2, file.path(tmpdir, "subset_results.sage.tsv"))
+
+bfcadd(rpx_cache,
+       rname = "sager_subset_id",
+       fpath = file.path(tmpdir, "subset_results.sage.tsv"),
+       action = "copy")
+
+## Subset sage quantitation results quant.tsv
+
+sager_quant2 <- read_tsv(sager_rpath["quant"]) |> ## has 14 variables
+    right_join(sager_id2, multiple = "all") |>
+    select(1:14)
+
+write_tsv(sager_quant2, file.path(tmpdir, "subset_quant.tsv"))
+
+bfcadd(rpx_cache,
+       rname = "sager_subset_quant",
+       fpath = file.path(tmpdir, "subset_quant.tsv"),
+       action = "copy")
+
+
+## Subset corresponding spectra
+
+sp <- Spectra(mzml_rpath)
+
+k <- spectraData(sp)[, c("spectrumId", "dataOrigin")] |>
+    data.frame() |>
+    rownames_to_column() |>
+    mutate(dataOrigin = basename(dataOrigin)) |>
+    rename(scannr = spectrumId) |>
+    rename(filename = dataOrigin) |>
+    right_join(sager_id2, multiple = "all") |>
+    pull(rowname)
+
+sp[as.numeric(k)] |>
+    export(MsBackendMzR(),
+           file = file.path(tmpdir, "sager_subset_PXD016766.mzML"))
+
+bfcadd(rpx_cache,
+       rname = "sager_subset_PXD016766",
+       fpath = file.path(tmpdir, "sager_subset_PXD016766.mzML"),
+       action = "copy")
